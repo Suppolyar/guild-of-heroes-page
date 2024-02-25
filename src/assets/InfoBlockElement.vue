ae6552d43eb2ea94fc69f4038fc9282920b4c5e3
Ilya Zakharov <74999345+Suppolyar@users.noreply.github.com>
2024-02-25 12:39:38 +0700

    feat: Modification layourt

---
+++ src/assets/InfoBlockElement.vue
@@ -0,0 +1,33 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="15"
+    height="15"
+    viewBox="0 0 15 15"
+    fill="none"
+  >
+    <rect
+      x="7.07129"
+      width="10"
+      height="10"
+      transform="rotate(45 7.07129 0)"
+      fill="#FDBF22"
+    />
+    <rect
+      x="7.07129"
+      y="4.20996"
+      width="4.04622"
+      height="4.04622"
+      transform="rotate(45 7.07129 4.20996)"
+      fill="#FFE39D"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "InfoBlockElement",
+});
+</script>
+
+<style scoped></style>
--- src/components/InfoBlock.vue
+++ src/components/InfoBlock.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="InfoBlock" :class="setCurrentBlockSize">
-    <div class="InfoBlock-line h-[6px] w-full bg-[#A56612]"></div>
+    <div class="InfoBlock-line relative h-[6px] w-full bg-[#A56612]">
+      <div class="absolute -left-2 -top-1"><InfoBlockElement /></div>
+      <div class="absolute -right-2 -top-1"><InfoBlockElement /></div>
+    </div>
     <div
       class="max-w-[580px] border-2 border-[#9E7641] bg-[#E0C094] px-[36px] py-[32px]"
     >
       <slot />
     </div>
-    <div class="InfoBlock-line h-[6px] w-full bg-[#A56612]"></div>
+    <div class="InfoBlock-line relative h-[6px] w-full bg-[#A56612]">
+      <div class="absolute -left-2 -top-1"><InfoBlockElement /></div>
+      <div class="absolute -right-2 -top-1"><InfoBlockElement /></div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import InfoBlockElement from "../assets/InfoBlockElement.vue";
+
 defineOptions({
   name: "InfoBlock",
 });
