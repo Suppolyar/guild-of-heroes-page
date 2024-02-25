7a382eee85fec9d5418a4ce88eea0f06dd91fdd1
Ilya Zakharov <74999345+Suppolyar@users.noreply.github.com>
2024-02-25 12:44:02 +0700

    feat: Script at onClickBtn

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,15 +1,23 @@
 <template>
   <button
+    @click="onBtnClick"
     class="rounded-2xl bg-gradient-to-br from-[#F6D621] via-[#FBCB4C] to-[#FFC734] px-[76px] py-[28px] text-[35px] font-bold text-[#825805] shadow-lg"
   >
-    Перейти
+    Скачать
   </button>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 defineOptions({
   name: "BaseButton",
 });
+
+const onBtnClick = () => {
+  window.__GEM.detectAndDownload(
+    "13.2013983",
+    "https://static.gc.my.games/HawkedLoader.exe",
+  );
+};
 </script>
 
 <style lang="scss" scoped></style>
