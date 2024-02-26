4d477afa0451b6399faa94521b51caaa63d5856b
Ilya Zakharov <74999345+Suppolyar@users.noreply.github.com>
2024-02-26 18:07:11 +0700

    fix

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,21 +1,12 @@
 <template>
   <div
-    class="h-screen w-screen overflow-hidden bg-guild-of-heroes bg-cover bg-center bg-no-repeat"
+    class="relative h-screen w-screen overflow-hidden bg-guild-of-heroes bg-cover bg-center bg-no-repeat"
   >
+    <MyGamesLogo class="absolute right-[80px] top-[80px]" />
     <div
       class="flex h-full flex-col items-center justify-center gap-5 py-20 text-lg 2xl:gap-10"
     >
       <MainLogo />
-      <InfoBlock size="large">
-        Игра больше не публикуется на площадке. Если вы играли в проект ранее,
-        перейдите на сайт разработчика и авторизуйтесь, чтобы сохранить своего
-        персонажа
-        <br />
-        <br />
-        Нажимая "Перейти" вы соглашаетесь с передачей ваших данных компании
-        MY.GAMES B.V. и их обработкой согласно Политике конфиденциальности
-        портала MY.GAMES
-      </InfoBlock>
       <BaseButton />
       <InfoBlock size="medium">
         Скачивая MY.GAMES Launcher вы соглашаетесь с передачей ваших данных
@@ -30,6 +21,7 @@
 import MainLogo from "../assets/main-logo.vue";
 import BaseButton from "../components/BaseButton.vue";
 import InfoBlock from "../components/InfoBlock.vue";
+import MyGamesLogo from "../assets/my-games-logo.vue";
 </script>
 
 <style scoped></style>
